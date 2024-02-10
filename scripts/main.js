aa79a1c063502cb6b7be68de86ccd43375e2fe93
jongowan <jonathankgowan@gmail.com>
2024-02-09 19:31:18 -0600

    card board works

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,39 @@
-const cardTable = document.getElementById('card-table')
-const cardSection = document.getElementById('card-section')
-const dealerSection = document.getElementById('dealer-section')
-const cardFilesPrefix = './images/cards/fronts/'
-const cardFilesSuitNames = [
-    'spades_', 'hearts_', 'clubs_', 'diamonds_'
-]
-const cardFilesDenomNames = [
-    '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'
-]
-const cardFilesSuffix = '.svg'
-const cardFiles = []
-const tableCardFiles = []
-const cardSeparationRatio = 0.05
-
-function getCardImgTag(suit, denom) {
-    const file = cardFilesPrefix + suit + denom + cardFilesSuffix
-    return '<img src="' + file + '" class="playing-card" alt="' + file + '">'
-}
+window.onload = function () {
+    const cardTable = document.getElementById('card-table')
+    const cardSection = document.getElementById('card-section')
+    const leftCardSection = document.getElementById('left-card-section')
+    const rightCardSection = document.getElementById('right-card-section')
+    const dealerSection = document.getElementById('dealer-section')
+    const cardFilesPrefix = './images/cards/fronts/'
+    const cardFilesSuitNames = [
+        'spades', 'hearts', 'clubs', 'diamonds'
+    ]
+    const cardFilesDenomNames = [
+        'ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'
+    ]
+    const cardFilesSuffix = '.svg'
+    const cardFiles = []
+    const tableCardFiles = []
 
 
-if (window.innerWidth < window.innerHeight) {
-    cardTable.classList.add('vp-tall')
-}
-else {
-    cardTable.classList.add('vp-wide')
-}
-
-for (let i = 0; i < cardFilesSuitNames.length; i++) {
-    const suit = cardFilesSuitNames[i]
-    for (let j = 0; j < cardFilesDenomNames.length; j++) {
-        const denom = cardFilesDenomNames[j]
-        const cardFile = getCardImgTag(suit, denom)
-
-        if (i == 0) {
-            tableCardFiles.push(cardFile)
-        }
-
-        cardFiles.push(cardFile)
+    if (window.innerWidth < window.innerHeight) {
+        cardTable.classList.add('vp-tall')
     }
+    else {
+        cardTable.classList.add('vp-wide')
+    }
+
+    window.onresize = function () {
+        const cardMaxWidth = cardSection.clientWidth / 7 / 1.15
+        const cardMaxHeight = cardSection.clientHeight / 2 / 1.15
+    
+        const rootStyle = document.querySelector(':root').style
+        rootStyle.setProperty('--card-max-width', cardMaxWidth + 'px')
+        rootStyle.setProperty('--card-max-height', cardMaxHeight + 'px')
+    }
+    window.onresize()
+
+    // for (const cardTag of document.getElementsByClassName('playing-card')) {
+    //     cardTag.style
+    // }
 }
-console.log(cardFiles)
-console.log(tableCardFiles)
